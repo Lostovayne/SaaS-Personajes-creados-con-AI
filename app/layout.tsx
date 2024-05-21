98c138def7cfe9e81141e423ccfdd73e385b81bc
LostoVayne <moises.barrera.dev@gmail.com>
2024-05-21 16:35:57 -0400

    agregado el searchInput

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
+        <body className={cn("bg-secondary ", inter.className)}>
           <ThemeProvider
             attribute="class"
             // forcedTheme="dark"
