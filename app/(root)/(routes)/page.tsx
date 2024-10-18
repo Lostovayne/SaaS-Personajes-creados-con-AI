06339336f74038d54cf33275fb861b6f11ae5694
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-17 23:37:33 -0300

    agrega el listado de personajes

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,41 @@
-import Categories from "@/components/categories";
-import { SearchInput } from "@/components/search-input";
-import prismadb from "@/lib/prismadb";
+import Categories from '@/components/categories';
+import Companions from '@/components/companions';
+import { SearchInput } from '@/components/search-input';
+import prismadb from '@/lib/prismadb';
 
-export default async function Home() {
+interface HomePageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+export default async function Home({ searchParams }: HomePageProps) {
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      _count: {
+        select: {
+          Message: true,
+        },
+      },
+    },
+  });
   const categories = await prismadb.category.findMany();
 
   return (
-    <div className="h-full p-4 space-y-2">
+    <div className='h-full p-4 space-y-2'>
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   );
 }
