04b1387e48425d948ad045bfa50b8527cd4ebd90
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-31 04:01:06 -0300

    agrega Chat

---
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -0,0 +1,50 @@
+import prismadb from '@/lib/prismadb';
+import { RedirectToSignIn } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { FC, ReactElement } from 'react';
+
+interface ChatIdPageProps {
+	params: {
+		chatId: string;
+	};
+}
+
+const ChatIdPage: FC<ChatIdPageProps> = async ({ params }: ChatIdPageProps): Promise<ReactElement | null> => {
+	const { userId } = auth();
+
+	if (!userId) {
+		return RedirectToSignIn({
+			redirectUrl: '/',
+		});
+	}
+
+	const companion = await prismadb.companion.findUnique({
+		where: {
+			id: params.chatId,
+		},
+		include: {
+			Message: {
+				orderBy: {
+					createdAt: 'asc',
+				},
+				where: {
+					userId,
+				},
+			},
+			_count: {
+				select: {
+					Message: true,
+				},
+			},
+		},
+	});
+
+	if (!companion) {
+		return redirect('/');
+	}
+
+	return <div>content</div>;
+};
+
+export default ChatIdPage;
---
+++ app/(chat)/layout.tsx
@@ -0,0 +1,3 @@
+export default function ChatLayout({ children }: { children: React.ReactNode }) {
+	return <div className='mx-auto max-w-4xl w-full'>{children}</div>;
+}
